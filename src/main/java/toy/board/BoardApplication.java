10321258c827a264c67551a43eddf8406070e6a9
Haaae <101785628+Haaae@users.noreply.github.com>
2023-08-04 18:53:02 +0900

    add: 오토 컨피그레이션에서 스프링 시큐리티 제외

--- src/main/java/toy/board/BoardApplication.java
+++ src/main/java/toy/board/BoardApplication.java
@@ -4,12 +4,13 @@ import java.util.Optional;
 import java.util.UUID;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)	// password encoder만 사용하기위해 스프링 시큐리티는 사용안함
 public class BoardApplication {
 
 	public static void main(String[] args) {

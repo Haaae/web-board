5c5d19c8db4f1cf9126b79ff2ca76fcc5f238004
Haaae <101785628+Haaae@users.noreply.github.com>
2023-08-07 20:57:46 +0900

    add: 세션에 로그인 멤버 정보가 존재하지 않는 공통 예외 추가

--- src/main/java/toy/board/exception/LoginException.java
+++
@@ -1,5 +0,0 @@
-package toy.board.exception;
-
-public interface LoginException {
-    static final String FIELD = "login";
-}
--- src/main/java/toy/board/exception/login/LoginException.java
+++ src/main/java/toy/board/exception/login/LoginException.java
@@ -1,7 +1,5 @@
 package toy.board.exception.login;
 
-import lombok.Getter;
-
 public interface LoginException {
     static final String FIELD = "login";
 }

7fe059c35a9899a68e0379c5025b0c6a160f523f
Haaae <101785628+Haaae@users.noreply.github.com>
2023-08-16 22:31:43 +0900

    modify: Redis 관련 CRUD 메서드를 RedisRepository로 분리
    - Mockito를 이용한 테스트 목적
    - DB 접근과 비즈니스 로직 분리

---
+++ src/main/java/toy/board/repository/RedisRepository.java
@@ -0,0 +1,26 @@
+package toy.board.repository;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class RedisRepository {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public Optional<String> getValues(final String key){
+        return Optional.ofNullable((String) redisTemplate.opsForValue().get(key));
+    }
+
+    public boolean deleteValues(final String key){
+        return Boolean.TRUE.equals(redisTemplate.delete(key));
+    }
+
+    public void setValues(final String key, final String value,final Long expiredTime){
+        redisTemplate.opsForValue().set(key, value, expiredTime, TimeUnit.MILLISECONDS);
+    }
+}
--- src/main/java/toy/board/service/RedisService.java
+++ src/main/java/toy/board/service/RedisService.java
@@ -6,16 +6,14 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import toy.board.repository.RedisRepository;
 
 @Service
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class RedisService {
 
-    private final RedisTemplate<String, Object> redisTemplate;
-
-    public void setValues(String key, String value,Long expiredTime){
-        redisTemplate.opsForValue().set(key, value, expiredTime, TimeUnit.MILLISECONDS);
-    }
+    private final RedisRepository redisRepository;
 
     /**
      *
@@ -23,17 +21,19 @@ public class RedisService {
      * @param values
      * @return return true if values exist in DB and same to parameter with delete it.
      */
+    @Transactional
     public boolean deleteIfExistAndSame(final String key, final String values) {
-        Optional<String> findValues = getValues(key);
-        deleteValues(key);  // 어차피 존재하지 않으면 아무 일도 일어나지 않으므로 시행
-        return values.equals(findValues.orElse(null));  // if found, the find values must not be null.
+        boolean isDeleted = false;
+        Optional<String> findValues = redisRepository.getValues(key);
+
+        if (isEquals(values, findValues)) {
+            isDeleted = redisRepository.deleteValues(key);
+        }
+
+        return isDeleted;
     }
 
-    public Optional<String> getValues(final String key){
-        return Optional.ofNullable((String) redisTemplate.opsForValue().get(key));
-    }
-
-    public void deleteValues(String key){
-        redisTemplate.delete(key);
+    private boolean isEquals(final String values, final Optional<String> findValues) {
+        return values.equals(findValues.orElse(null));
     }
 }

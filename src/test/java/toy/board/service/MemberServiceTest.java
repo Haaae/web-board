d171ccff90589430a24ae014f151cc49c776056f
Haaae <101785628+Haaae@users.noreply.github.com>
2023-08-19 00:44:07 +0900

    test: memberService 중복 검증 메서드 테스트

--- src/test/java/toy/board/service/MemberServiceTest.java
+++ src/test/java/toy/board/service/MemberServiceTest.java
@@ -1,62 +1,106 @@
 package toy.board.service;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import toy.board.entity.auth.Login;
 import toy.board.entity.user.LoginType;
 import toy.board.entity.user.Member;
 import toy.board.entity.user.Profile;
 import toy.board.entity.user.UserRole;
-import toy.board.repository.LoginRepository;
-import toy.board.repository.MemberRepository;
-import toy.board.repository.ProfileRepository;
+import toy.board.exception.BusinessException;
+import toy.board.exception.ExceptionCode;
+import toy.board.repository.member.MemberRepository;
 
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.*;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)   // 사용하지 않는 Mock 설정에 대해 오류를 발생하지 않도록 설정
 class MemberServiceTest {
 
-    @Autowired
-    MemberRepository memberRepository;
-    @Autowired
-    LoginRepository loginRepository;
-    @Autowired
-    ProfileRepository profileRepository;
+    @InjectMocks
+    private MemberService memberService;
+
+    @Mock
+    private MemberRepository memberRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+
 
     String username = "name";
-    Login login = Login.builder().encodedPassword("password").build();
-    Profile profile = Profile.builder().nickname("nickname").build();
     LoginType loginType = LoginType.LOCAL_LOGIN;
     UserRole userRole = UserRole.USER;
+    String password = "password";
+    String nickname = "nickname";
+    Member member;
+    Login login;
+    Profile profile;
 
-    @Transactional
-    @Test
-    void save() {
-        // give
-        Profile profile = Profile.builder().nickname("nickname").build();
-        Login login = new Login("password");
-        Member member = new Member(username, login, profile, loginType, userRole);
+    @BeforeEach
+    void init() {
+        this.profile = Profile.builder().nickname(nickname).build();
+        this.login = new Login(password);
+        this.member = new Member(username, login, profile, loginType, userRole);
 
         member.changeLogin(login);
-
-        // cascade 설정을 통해 member가 의존하는 profile과 login이 영속성 컨텍스트에 등록되지 않아도 member를 등록할 때 자동으로 등록된다.
-        // 실제로 쿼리를 보면 profile과 login의 insert 쿼리가 member보다 먼저 요청된다.
-//        profileRepository.save(profile);
-//        loginRepository.save(login);
         memberRepository.save(member);
+        given(memberRepository.save(any())).willReturn(member);
+        given(memberRepository.existsByUsername(eq(username))).willReturn(false);
+        given(memberRepository.existsByUsername(eq("wrong input"))).willReturn(true);
+        given(memberRepository.existsByNickname(eq(nickname))).willReturn(false);
+        given(memberRepository.existsByNickname(eq("wrong input"))).willReturn(true);
+
+        given(passwordEncoder.encode(anyString())).willReturn(password);
+    }
+
+
+    // join
+    // - 중복 검사가 잘 이루어지는가
+
+    @DisplayName("이메일과 닉네임이 모두 기존에 존재하지 않음: 통과")
+    @Test
+    public void MemberServiceTest() throws  Exception {
+        //given
 
         //when
-        Optional<Member> findMember = memberRepository.findMemberByUsername(member.getUsername());
-        Optional<Login> findLogin = findMember.map(Member::getLogin);
-        System.out.println("findLogin = " + findLogin);
-        System.out.println("login = " + login);
+        Member result = memberService.join(username, password, nickname);
+        //then
+        assertThat(result.getUsername()).isEqualTo(member.getUsername());
+    }
+    
+    @DisplayName("이메일이 기존 이메일과 중복일 경우: 예외발생")
+    @Test
+    public void join_fail_cause_duplicate_username() throws  Exception {
+        //given
+        String wrongInput = "wrong input";
+        //when
+        //then
+        BusinessException e = assertThrows(BusinessException.class, () -> memberService.join(wrongInput, password, nickname));
+        assertThat(e.getCode()).isEqualTo(ExceptionCode.DUPLICATE_USERNAME);
+    }
 
-        // then
-        assertThat(findLogin.isEmpty()).isFalse();
-        assertThat(findLogin.get().getPassword()).isEqualTo(login.getPassword());
+    @DisplayName("닉네임이 기존 닉네임과 중복일 경우: 예외발생")
+    @Test
+    public void join_fail_cause_duplicate_nickname() throws  Exception {
+        //given
+        String wrongInput = "wrong input";
+
+        //when
+        //then
+        BusinessException e = assertThrows(BusinessException.class, () -> memberService.join(username, password, wrongInput));
+        assertThat(e.getCode()).isEqualTo(ExceptionCode.DUPLICATE_NICKNAME);
     }
 }

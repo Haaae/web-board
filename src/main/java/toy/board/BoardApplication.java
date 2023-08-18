2c247d7adefb43613017d7822b4c3dcbe2b41070
Haaae <101785628+Haaae@users.noreply.github.com>
2023-08-18 19:49:34 +0900

    delete: springConfig로 위임한 메서드 삭제

--- src/main/java/toy/board/BoardApplication.java
+++ src/main/java/toy/board/BoardApplication.java
@@ -1,12 +1,8 @@
 package toy.board;
 
-import java.util.Optional;
-import java.util.UUID;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -15,12 +11,5 @@ public class BoardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
-	}
-
-	// @CreateBy, @LastModifiedBy를 위한 사용자 전달 함수
-	@Bean
-	public AuditorAware<String> auditorProvider() {
-		// TODO: 2023-07-13 spring security의 session에서 ID 등을 가져와 createBy, lastModifiedBy를 수정할 수 있도록 한다.
-		return () -> Optional.of(UUID.randomUUID().toString());
 	}
 }

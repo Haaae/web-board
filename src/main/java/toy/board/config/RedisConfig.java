95a82740a037d219fe12878f06768ca84aaaa36c
Haaae <101785628+Haaae@users.noreply.github.com>
2023-08-21 20:03:57 +0900

    feat: 순환참조 제거

--- src/main/java/toy/board/config/RedisConfig.java
+++ src/main/java/toy/board/config/RedisConfig.java
@@ -19,7 +19,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 @EnableTransactionManagement // @Transaction을 사용하기 위함
 public class RedisConfig {
-    private RedisConnectionFactory redisConnectionFactory;
 
     @Value("${spring.data.redis.host}")
     private String host;
@@ -27,30 +26,25 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
-/*
- * 순환 참조에 기인한 오류 발생.
- * 순환 참조 아닌듯
- * 흠..
- * ===========
- * RedisConfig가 redisConnectionFactory를 필드멤버로 갖고 생성자로 의존성 주입을 받으면,
- * RedisConfig의 redisConnectionFactory() 메서드로 Bean이 등록되어야 하므로 RedisConfig가 미리 생성되어야 하는
- * 패러독스가 발생
- * => @PostConstruct나 생성자를 통해 redisConnectionFactory를 스프링 빈이 아닌 일반 변수로 초기화하면 됨
- */
-//    @Bean
-//    public RedisConnectionFactory redisConnectionFactory() {
-//        return new LettuceConnectionFactory(host, port);
-//    }
-
-    @PostConstruct
-    public void init() {
-        this.redisConnectionFactory = new LettuceConnectionFactory(host, port);
+    /*
+     * 순환 참조에 기인한 오류 발생.
+     * 순환 참조 아닌듯
+     * 흠..
+     * ===========
+     * RedisConfig가 redisConnectionFactory를 필드멤버로 갖고 생성자로 의존성 주입을 받으면,
+     * RedisConfig의 redisConnectionFactory() 메서드로 Bean이 등록되어야 하므로 RedisConfig가 미리 생성되어야 하는
+     * 패러독스가 발생
+     * => 주입받지 말고 메서드로 받자
+     */
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
     public RedisTemplate<?,?> redisTemplate() {
         RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
+        template.setConnectionFactory(redisConnectionFactory());
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
         return template;
@@ -59,7 +53,7 @@ public class RedisConfig {
     @Bean
     public StringRedisTemplate stringRedisTemplate() {
         StringRedisTemplate template = new StringRedisTemplate();
-        template.setConnectionFactory(redisConnectionFactory);
+        template.setConnectionFactory(redisConnectionFactory());
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
         return template;

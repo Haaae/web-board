2d0b0817519dec07e4b937f3c4114365f2428698
Haaae <101785628+Haaae@users.noreply.github.com>
2023-08-21 17:01:40 +0900

    feat: password encoder를 BCryptPasswordEncoder로 명시

--- src/main/java/toy/board/config/SecurityConfig.java
+++ src/main/java/toy/board/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package toy.board.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +10,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 }

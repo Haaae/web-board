e6aa80f702d054fc5d0c940c82fabeb38e6a8624
Haaae <101785628+Haaae@users.noreply.github.com>
2023-08-29 09:30:31 +0900

    feat: findPostDtoById 기능

--- src/main/java/toy/board/repository/post/PostQueryRepository.java
+++ src/main/java/toy/board/repository/post/PostQueryRepository.java
@@ -1,10 +1,13 @@
 package toy.board.repository.post;
 
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import toy.board.controller.post.dto.PostListDto;
+import toy.board.controller.post.dto.PostDto;
 
 public interface PostQueryRepository {
 
-    Page<PostListDto> findAllPost(Pageable pageable);
+    Page<PostDto> findAllPost(Pageable pageable);
+
+    Optional<PostDto> findPostDtoById(Long postId);
 }
--- src/main/java/toy/board/repository/post/PostRepositoryImpl.java
+++ src/main/java/toy/board/repository/post/PostRepositoryImpl.java
@@ -5,10 +5,13 @@ import static toy.board.domain.post.QPost.post;
 import static toy.board.domain.user.QMember.member;
 import static toy.board.domain.user.QProfile.profile;
 
+import com.querydsl.core.types.ConstructorExpression;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import toy.board.controller.post.dto.PostListDto;
+import toy.board.controller.post.dto.PostDto;
 import toy.board.domain.post.Post;
 import toy.board.repository.support.Querydsl4RepositorySupport;
 
@@ -19,22 +22,11 @@ public class PostRepositoryImpl extends Querydsl4RepositorySupport implements Po
     }
 
     @Override
-    public Page<PostListDto> findAllPost(Pageable pageable) {
+    public Page<PostDto> findAllPost(Pageable pageable) {
         return applyPagination(pageable,
                 contentQuery -> contentQuery
-                        .select(Projections.constructor(PostListDto.class,
-                                post.id.as("postId"),
-                                member.id.as("writerId"),
-                                profile.nickname.as("writer"),
-                                post.title,
-                                post.content,
-                                post.hits,
-                                post.createdDate,
-                                comment.count().as("commentNum")
-                        ))
+                        .select(getPostDtoConstructorExpression())
                         .from(post)
-                        .leftJoin(post.member, member)
-                        .leftJoin(member.profile, profile)
                         .leftJoin(comment).on(comment.post.eq(post))
                         .groupBy(post)
                         .offset(pageable.getOffset())
@@ -43,6 +35,36 @@ public class PostRepositoryImpl extends Querydsl4RepositorySupport implements Po
                 countQuery -> countQuery
                         .select(post.count())
                         .from(post)
+        );
+    }
+
+    @Override
+    public Optional<PostDto> findPostDtoById(Long postId) {
+        return Optional.ofNullable(
+                select(getPostDtoConstructorExpression())
+                        .from(post)
+                        .where(post.id.eq(postId))
+                        .leftJoin(comment).on(comment.post.eq(post))
+                        .groupBy(post)
+                        .fetchOne()
+        );
+    }
+
+    private ConstructorExpression<PostDto> getPostDtoConstructorExpression() {
+        return Projections.constructor(PostDto.class,
+                post.id.as("postId"),
+                post.writerId,
+                post.writer,
+                post.title,
+                post.content,
+                post.hits,
+                post.createdDate,
+                new CaseBuilder()
+                        .when(post.createdDate.eq(post.lastModifiedDate))
+                        .then(false)
+                        .otherwise(true)
+                        .as("isModified"),
+                comment.count().as("commentNum")
         );
     }
 }

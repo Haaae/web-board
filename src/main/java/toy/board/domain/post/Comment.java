c9c2ae760228683ec42d44fa3716209591a058b1
Haaae <101785628+Haaae@users.noreply.github.com>
2023-08-29 09:26:31 +0900

    feat: comment type이 reply를 갖는 계층형 구조로 구현 및 writerId와 writer를 필드멤버로 갖도록 변경

--- src/main/java/toy/board/domain/post/Comment.java
+++ src/main/java/toy/board/domain/post/Comment.java
@@ -1,7 +1,10 @@
 package toy.board.domain.post;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -31,33 +34,50 @@ public class Comment extends BaseDeleteEntity {
     @Column(name = "type", nullable = false, updatable = false)
     private CommentType type;
 
-    @Column(name = "bundle_id", unique = true, updatable = false)
-    private String bundleId;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id", nullable = false, updatable = false)
     private Post post;
 
+    @Column(name = "writer_id") // 작성자가 탈퇴할 경우 null로 변경해야 한다.
+    private Long wtiterId;
+
+    @Column(name = "writer")    // 작성자가 탈퇴할 경우 null로 변경해야 한다.
+    private String writer;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Comment> replies = new ArrayList<>();
+
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id", nullable = false, updatable = false)
-    private Member member;
+    @JoinColumn(name = "parent_comment_id", updatable = false)
+    private Comment parent;
 
     /**
      * 양방향 관계인 Member와 Post에 대해 자동으로 양방향 매핑을 수행한다.
      */
     public Comment(
             @NotNull final Post post,
-            @NotNull final Member member,
+            @NotNull final Long writerId,
+            @NotBlank final String writer,
             @NotNull final String content,
-            @NotNull final CommentType type,
-            @NotNull final String bundleId
+            @NotNull final CommentType type
     ) {
 
         this.post = post;
-        this.member = member;
+        this.wtiterId = writerId;
+        this.writer = writer;
         this.content = content;
         this.type = type;
-        this.bundleId = bundleId;
+    }
+
+    public Comment(
+            @NotNull final Post post,
+            @NotNull final Long writerId,
+            @NotBlank final String writer,
+            @NotNull final String content,
+            @NotNull final Comment comment
+    ) {
+        this(post, writerId, writer, content, CommentType.REPLY);
+        comment.leaveReply(this);
     }
 
     public boolean update(final String content) {
@@ -67,5 +87,34 @@ public class Comment extends BaseDeleteEntity {
 
         this.content = content;
         return true;
+    }
+
+    public void leaveReply(Comment reply) {
+        if (areTypesCorrectThisAnd(reply)) {
+            throw new IllegalArgumentException("주어진 댓글과 대댓글의 타입이 올바르지 않습니다.");
+        }
+
+        if (hasComment(reply)) {
+            throw new IllegalArgumentException("대댓글이 이미 다른 댓글에 포섭되어 있습니다.");
+        }
+
+        if (isNew(reply)) {
+            throw new IllegalArgumentException("댓글이 이미 해당 대댓글을 포함하고 있습니다.");
+        }
+
+        this.replies.add(reply);
+        reply.parent = this;
+    }
+
+    private boolean isNew(Comment reply) {
+        return this.replies.contains(reply);
+    }
+
+    private static boolean hasComment(Comment reply) {
+        return reply.parent != null;
+    }
+
+    private boolean areTypesCorrectThisAnd(Comment reply) {
+        return this.type != CommentType.COMMENT || reply.type != CommentType.REPLY;
     }
 }

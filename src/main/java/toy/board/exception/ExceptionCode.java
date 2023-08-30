50579fe74d6457890b2b679ad6d985cc1b137dad
Haaae <101785628+Haaae@users.noreply.github.com>
2023-08-30 14:01:45 +0900

    chore: 오타 수정

--- src/main/java/toy/board/exception/ExceptionCode.java
+++ src/main/java/toy/board/exception/ExceptionCode.java
@@ -18,7 +18,7 @@ public enum ExceptionCode {
 
     POST_NOT_FOUND(404, "POST-001", "게시물을 찾을 수 없음"),
 
-    COMMENT_NOT_FOUND(404, "COMENT-001", "댓글을 찾을 수 없음");
+    COMMENT_NOT_FOUND(404, "COMMENT-001", "댓글을 찾을 수 없음");
 
     private final int status;
     private final String code;

81189ca008dfbc4ea4be0b6721beb8f61f6d9247
Haaae <101785628+Haaae@users.noreply.github.com>
2023-08-30 15:52:24 +0900

    feat: post을 갖고 있는 comment를 모두 삭제하는 메서드 추가

--- src/main/java/toy/board/repository/comment/CommentRepository.java
+++ src/main/java/toy/board/repository/comment/CommentRepository.java
@@ -2,7 +2,11 @@ package toy.board.repository.comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import toy.board.domain.post.Comment;
+import toy.board.domain.post.Post;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentQueryRepository {
 
+    void deleteCommentsByPost(Post post);
 }

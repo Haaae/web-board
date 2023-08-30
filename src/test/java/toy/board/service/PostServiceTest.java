aaeaf272649fac578aa80b350814f026bf658c2f
Haaae <101785628+Haaae@users.noreply.github.com>
2023-08-30 13:29:59 +0900

    test: post create 테스트 구현

---
+++ src/test/java/toy/board/service/PostServiceTest.java
@@ -0,0 +1,42 @@
+package toy.board.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import toy.board.domain.user.Member;
+import toy.board.domain.user.MemberTest;
+
+@SpringBootTest
+@Transactional
+class PostServiceTest {
+
+    @Autowired
+    private PostService postService;
+    @Autowired
+    private EntityManager em;
+
+    @DisplayName("post create의 반환값이 정상적으로 반환됨")
+    @Test
+    public void PostServiceTest() throws  Exception {
+        //given
+        Member member = MemberTest.create();
+        em.persist(member);
+        em.flush(); em.clear();
+        //when
+        Long postId = postService.create("title", "content", member.getId());
+
+        //then
+        System.out.println("postId = " + postId);
+        assertThat(postId).isNotNull();
+        assertThat(postId).isEqualTo(1);
+    }
+
+
+}

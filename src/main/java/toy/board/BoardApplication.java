64eca858dc4517f7385918c6349489fa96f299c9
Haaae <101785628+Haaae@users.noreply.github.com>
2023-09-19 21:29:21 +0900

    style: 주석 변경

--- src/main/java/toy/board/BoardApplication.java
+++ src/main/java/toy/board/BoardApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing	// 생성일 수정일 자동으로 기록하기 위한 어노테이션
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)	// password encoder만 사용하기위해 스프링 시큐리티는 사용안함
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)	// password encoder만 사용하기 위해 스프링 시큐리티는 사용 안함 설정
 public class BoardApplication {
 
 	public static void main(String[] args) {

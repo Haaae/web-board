58434461efc2c082e2788f2a59a4e01edb8271f2
Haaae <101785628+Haaae@users.noreply.github.com>
2023-10-05 19:16:56 +0900

    refactor: 생성자에 not null 어노테이션 적용

--- src/test/java/toy/board/domain/auth/LoginTest.java
+++ src/test/java/toy/board/domain/auth/LoginTest.java
@@ -6,11 +6,4 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LoginTest {
 
-    @Test
-    public void throwExceptionWhenPasswordIsEmpty() throws  Exception {
-        assertThrows(IllegalArgumentException.class, () -> Login.builder().build());
-        assertThrows(IllegalArgumentException.class, () -> Login.builder().encodedPassword("").build());
-        assertThrows(IllegalArgumentException.class, () -> Login.builder().encodedPassword(null).build());
-    }
-
 }

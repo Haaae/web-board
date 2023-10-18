326b45a6ab2ddef9210b7b95389aa08c1cbc358d
Haaae <101785628+Haaae@users.noreply.github.com>
2023-10-18 19:47:31 +0900

    feat: static 생성 메서드 구현
    - service 단에서 post와 commentDtos를 PostDto로 변환하기 위해 생성 메서드 구현

--- src/main/java/toy/board/repository/post/dto/PostDto.java
+++ src/main/java/toy/board/repository/post/dto/PostDto.java
@@ -1,6 +1,10 @@
 package toy.board.repository.post.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import toy.board.domain.post.Post;
+import toy.board.repository.comment.dto.CommentDto;
 
 public record PostDto(
         Long postId,
@@ -13,4 +17,23 @@ public record PostDto(
         boolean isModified,
         Long commentNum
 ) {
+    public static PostDto of(Post post, List<CommentDto> commentDtos) {
+        AtomicLong commentNum = new AtomicLong(commentDtos.size());
+
+        commentDtos.forEach(c ->
+                commentNum.addAndGet(c.replies().size())
+        );
+
+        return new PostDto(
+                post.getId(),
+                post.getWriterId(),
+                post.getWriter(),
+                post.getTitle(),
+                post.getContent(),
+                post.getHits(),
+                post.getCreatedDate(),
+                post.isModified(),
+                commentNum.get()
+        );
+    }
 }

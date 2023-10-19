4db04be4289aea3291c15e365bf65b5a06e1a85b
Haaae <101785628+Haaae@users.noreply.github.com>
2023-10-19 11:59:31 +0900

    fix: isModified 판별 중 createdDate에 대한 null 검사 로직 추가

--- src/main/java/toy/board/domain/base/BaseTimeEntity.java
+++ src/main/java/toy/board/domain/base/BaseTimeEntity.java
@@ -22,6 +22,6 @@ public class BaseTimeEntity {
     private LocalDateTime lastModifiedDate;
 
     public boolean isModified() {
-        return !createdDate.isEqual(lastModifiedDate);
+        return createdDate != null && !createdDate.isEqual(lastModifiedDate);
     }
 }

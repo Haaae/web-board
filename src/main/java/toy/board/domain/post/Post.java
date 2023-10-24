61d2acd382815edec91db008cb653563a7414e20
Haaae <101785628+Haaae@users.noreply.github.com>
2023-10-24 16:32:49 +0900

    feat: Comment 컬렉션을 갖도록 매핑하고, Member와 Comment 매핑 간소화 메서드 구현

--- src/main/java/toy/board/domain/post/Post.java
+++ src/main/java/toy/board/domain/post/Post.java
@@ -1,12 +1,25 @@
 package toy.board.domain.post;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
-import toy.board.domain.base.BaseDeleteEntity;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import toy.board.domain.base.BaseEntity;
 import toy.board.domain.user.Member;
-import toy.board.domain.user.Profile;
 import toy.board.exception.BusinessException;
 import toy.board.exception.ExceptionCode;
 
@@ -14,7 +27,8 @@ import toy.board.exception.ExceptionCode;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
-public class Post extends BaseDeleteEntity {
+@ToString(exclude = {"comments"})
+public class Post extends BaseEntity {
 
     public static final int TITLE_MAX_LENGTH = 50;
     public static final int CONTENT_MAX_LENGTH = 10000;
@@ -37,30 +51,37 @@ public class Post extends BaseDeleteEntity {
     @JoinColumn(name = "writer_id")
     private Member writer;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "writer")
-//    private Profile writer;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "post", orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
 
     public Post(
-//            @NotNull final Member writerId,
             @NotNull final Member writer,
             @NotBlank final String title,
             @NotBlank final String content
     ) {
 
-        this.writer = writer;
+        addPostTo(writer);
         this.title = title;
         this.content = content;
         this.hits = 0L;
     }
 
-    public void update(@NotBlank final String content, @NotNull final Long writer) {
+    /**
+     * Comment와 Post의 양방향 매핑을 위한 메서드.
+     * Comment에서 호출한다.
+     * @param comment Post에 포함될 새로운 메서드
+     */
+    public void addComment(Comment comment) {
+        this.comments.add(comment);
+    }
+
+    public void update(@NotBlank final String content, @NotNull final Member writer) {
         validateRight(writer);
         this.content = content;
     }
 
-    public void validateRight(final Long writer) {
-        if (!writer.equals(this.writer.getId())) {
+    public void validateRight(final Member writer) {
+        if (this.writer == null || !writer.getId().equals(this.writer.getId())) {
             throw new BusinessException(ExceptionCode.POST_NOT_WRITER);
         }
     }
@@ -75,5 +96,14 @@ public class Post extends BaseDeleteEntity {
 
     public String getWriterNickname() {
         return this.writer.getProfile().getNickname();
+    }
+
+    /**
+     * Post와 Member의 양방향 매핑을 위한 메서드.
+     * @param writer Post 작성자.
+     */
+    private void addPostTo(Member writer) {
+        this.writer = writer;
+        writer.addPost(this);
     }
 }

19df61295e03a3440d2bd5f335933a48ac657061
Haaae <101785628+Haaae@users.noreply.github.com>
2023-11-01 18:57:14 +0900

    add: replies가 null인 CommentDto를 생성하는 전역 메서드 추가

--- src/main/java/toy/board/repository/comment/dto/CommentDto.java
+++ src/main/java/toy/board/repository/comment/dto/CommentDto.java
@@ -1,5 +1,6 @@
 package toy.board.repository.comment.dto;
 
+import toy.board.domain.post.Comment;
 import toy.board.domain.post.CommentType;
 
 import java.time.LocalDateTime;
@@ -15,6 +16,27 @@ public record CommentDto(
         LocalDateTime createdDate,
         CommentListDto replies
 ) {
+
+    /**
+     * Comment의 프로퍼티로 CommentDto를 생성해 반환한다. 이때 Comment.replies에 접근하지 않고 CommentDto.replies를 null로 설정한다.
+     *
+     * @param comment
+     * @return replies가 null인 CommentDto
+     */
+    public static CommentDto createHasNotReplies(Comment comment) {
+        return new CommentDto(
+                comment.getId(),
+                comment.getWriterId(),
+                comment.getWriterNickname(),
+                comment.getContent(),
+                comment.getType(),
+                comment.isDeleted(),
+                comment.isModified(),
+                comment.getCreatedDate(),
+                null
+        );
+    }
+
     public boolean isCommentType() {
         return type == CommentType.COMMENT;
     }

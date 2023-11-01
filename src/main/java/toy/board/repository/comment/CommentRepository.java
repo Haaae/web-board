b31a6edeaa640992cb5504d011c67807f8b365c1
Haaae <101785628+Haaae@users.noreply.github.com>
2023-11-01 18:58:56 +0900

    add: 특정 memberId에 해당하는 writer를 가진 comment 페이징하는 메서드 추가

--- src/main/java/toy/board/repository/comment/CommentRepository.java
+++ src/main/java/toy/board/repository/comment/CommentRepository.java
@@ -1,6 +1,7 @@
 package toy.board.repository.comment;
 
-import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,12 +9,15 @@ import toy.board.domain.post.Comment;
 import toy.board.domain.post.CommentType;
 import toy.board.domain.post.Post;
 
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentQueryRepository {
 
     /**
      * Comment 반환 시 Member, Profile을 fetch join한다.
      * ~ToOne 매핑관계에 대한 fetch join은 별명을 사용할 수 있고,
      * 연계하여 fetch join할 수 있다.
+     *
      * @param id must not be {@literal null}.
      * @return
      */
@@ -23,6 +27,22 @@ public interface CommentRepository extends JpaRepository<Comment, Long>, Comment
             + "WHERE c.id = :commentId"
     )
     Optional<Comment> findCommentById(@Param("commentId") final Long id);
+
+    /**
+     * WriterId가 memberId와 같은 Comment를 페이징 처리하여 Page<Post>로 반환한다. 이때 Member와 Profile을 fetch join한다.
+     *
+     * @param writerId writerId가 일치하는 Post들을 반환한다.
+     * @param pageable 페이징 정보
+     */
+    @Query(value = """
+            SELECT c FROM Comment c 
+            LEFT JOIN FETCH c.writer AS w 
+            LEFT JOIN FETCH w.profile 
+            WHERE w.id = :writerId
+            """,
+            countQuery = "SELECT count(c) FROM Comment c WHERE c.writer.id = :memberId"
+    )
+    Page<Comment> findAllByWriterId(@Param("writerId") Long writerId, Pageable pageable);
 
     void deleteCommentsByPost(final Post post);
 

2649f1741342457971b317008d241a373c987305
Haaae <101785628+Haaae@users.noreply.github.com>
2023-11-01 19:02:45 +0900

    feat: 내정보 페이지 정보를 전달하는 dto 구현

---
+++ src/main/java/toy/board/controller/dto/MyPageDto.java
@@ -0,0 +1,19 @@
+package toy.board.controller.dto;
+
+import toy.board.domain.user.Member;
+
+public record MyPageDto(
+        String username,
+        String nickname,
+        long postCount,
+        long commentCount
+) {
+    public static MyPageDto of(Member member) {
+        return new MyPageDto(
+                member.getUsername(),
+                member.getNickname(),
+                member.getPostCount(),
+                member.getCommentCount()
+        );
+    }
+}

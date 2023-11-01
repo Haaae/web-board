c69d0ebc574879bd031e3ca680e66fd5281c318f
Haaae <101785628+Haaae@users.noreply.github.com>
2023-11-01 14:36:33 +0900

    style: import문 최적화

--- src/main/java/toy/board/domain/post/Comment.java
+++ src/main/java/toy/board/domain/post/Comment.java
@@ -1,27 +1,16 @@
 package toy.board.domain.post;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import toy.board.domain.base.BaseDeleteEntity;
 import toy.board.domain.user.Member;
 import toy.board.exception.BusinessException;
 import toy.board.exception.ExceptionCode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -120,9 +109,9 @@ public class Comment extends BaseDeleteEntity {
     }
 
     public String getWriterNickname() {
-        return this.writer.getProfile().getNickname();
+        return this.writer.getNickname();
     }
-    
+
     public void applyWriterWithdrawal() {
         this.writer = null;
     }

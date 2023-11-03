43f47a3533e41b16b46f2b8a080455ca466c3cb3
Haaae <101785628+Haaae@users.noreply.github.com>
2023-11-04 00:09:03 +0900

    test: findAllByWriterId() 시 삭제상태인 Comment는 가져오지 않음 테스트 코드 작성

---
+++ src/test/java/toy/board/repository/comment/CommentRepositoryTest.java
@@ -0,0 +1,103 @@
+package toy.board.repository.comment;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
+import toy.board.domain.auth.Login;
+import toy.board.domain.post.Comment;
+import toy.board.domain.post.CommentType;
+import toy.board.domain.post.Post;
+import toy.board.domain.user.LoginType;
+import toy.board.domain.user.Member;
+import toy.board.domain.user.Profile;
+import toy.board.domain.user.UserRole;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class CommentRepositoryTest {
+
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private EntityManager em;
+
+    @DisplayName("findAllByWriterId() 시 삭제 상태인 Comment는 가져오지 않음")
+    @Test
+    public void 실행테스트_삭제된_댓글_제외() throws Exception {
+        //given
+        Long writerId = setup();
+
+        int size = 10;
+        int pageNum = 0;
+        int totalPages = 1;
+        int totalElements = 3;
+        String sort = "createdDate";
+
+        PageRequest pageable = PageRequest.of(pageNum, size, Sort.by(sort));
+
+        //when
+        Page<Comment> page = commentRepository.findAllByWriterId(writerId, pageable);
+
+        //then
+        assertThat(page.getSize()).isEqualTo(size);
+        assertThat(page.getTotalElements()).isEqualTo(totalElements);
+        assertThat(page.getTotalPages()).isEqualTo(totalPages);
+        for (Comment findComment : page.getContent()) {
+            assertThat(findComment.isCommentType()).isFalse();
+            System.out.println("findComment = " + findComment);
+        }
+    }
+
+    private Long setup() {
+        Member member = persistMember();
+
+        Post post = persistPost(member);
+
+        Comment comment = new Comment(post, member, "title", CommentType.COMMENT, null);
+        comment.delete();
+        Long writer = comment.getWriterId();
+        em.persist(comment);
+
+        Comment reply1 = new Comment(post, member, "title", CommentType.REPLY, comment);
+        Comment reply2 = new Comment(post, member, "title", CommentType.REPLY, comment);
+        Comment reply3 = new Comment(post, member, "title", CommentType.REPLY, comment);
+        em.persist(reply1);
+        em.persist(reply2);
+        em.persist(reply3);
+
+        em.flush();
+        em.clear();
+
+        return writer;
+    }
+
+    private Post persistPost(Member member) {
+        Post post = new Post(
+                member,
+                "title",
+                "content"
+        );
+        em.persist(post);
+        return post;
+    }
+
+    private Member persistMember() {
+        Member member = Member.builder(
+                "member",
+                new Login("password"),
+                Profile.builder("nickname").build(),
+                LoginType.LOCAL_LOGIN,
+                UserRole.USER
+        ).build();
+        em.persist(member);
+        return member;
+    }
+}

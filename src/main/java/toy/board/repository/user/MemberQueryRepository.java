d4d3e4d57863c2ca4a33f672cce547faa73c5f17
Haaae <101785628+Haaae@users.noreply.github.com>
2023-11-05 23:47:16 +0900

    refactor: spring data jpa -> querydsl로 마이그레이션

--- src/main/java/toy/board/repository/user/MemberQueryRepository.java
+++ src/main/java/toy/board/repository/user/MemberQueryRepository.java
@@ -1,6 +1,14 @@
 package toy.board.repository.user;
 
+import toy.board.domain.user.Member;
+
+import java.util.Optional;
+
 public interface MemberQueryRepository {
 
-    boolean existsByNickname(String nickname);
+    Optional<Member> findMemberByUsernameWithFetchJoinLogin(final String username);
+
+    Optional<Member> findMemberWithFetchJoinProfile(final Long id);
+
+    boolean existsByNickname(final String nickname);
 }
--- src/main/java/toy/board/repository/user/MemberRepository.java
+++ src/main/java/toy/board/repository/user/MemberRepository.java
@@ -1,18 +1,9 @@
 package toy.board.repository.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import toy.board.domain.user.Member;
 
-import java.util.Optional;
-
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberQueryRepository {
-
-    Optional<Member> findMemberByUsernameWithFetchJoinLogin(String username);
-
-    @Query(value = "SELECT m FROM Member m LEFT Join Fetch m.profile WHERE m.id = :memberId")
-    Optional<Member> findMemberWithFetchJoinProfile(@Param("memberId") Long id);
 
     boolean existsByUsername(String username);
 }

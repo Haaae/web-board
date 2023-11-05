1ac0900666a70faac1369304165d64114bfcadb1
Haaae <101785628+Haaae@users.noreply.github.com>
2023-11-05 23:48:04 +0900

    feat: spring data jpa -> querydsl로 마이그레이션한 메서드 구현

--- src/main/java/toy/board/repository/user/MemberRepositoryImpl.java
+++ src/main/java/toy/board/repository/user/MemberRepositoryImpl.java
@@ -3,6 +3,8 @@ package toy.board.repository.user;
 import toy.board.domain.user.Member;
 import toy.board.repository.support.Querydsl4RepositorySupport;
 
+import java.util.Optional;
+
 import static toy.board.domain.user.QMember.member;
 
 public class MemberRepositoryImpl extends Querydsl4RepositorySupport implements
@@ -13,7 +15,26 @@ public class MemberRepositoryImpl extends Querydsl4RepositorySupport implements
     }
 
     @Override
+    public Optional<Member> findMemberByUsernameWithFetchJoinLogin(String username) {
+        return Optional.ofNullable(
+                selectFrom(member)
+                        .leftJoin(member.login).fetchJoin()
+                        .where(member.username.eq(username))
+                        .fetchFirst()
+        );
+    }
 
+    @Override
+    public Optional<Member> findMemberWithFetchJoinProfile(Long id) {
+        return Optional.ofNullable(
+                selectFrom(member)
+                        .leftJoin(member.profile).fetchJoin()
+                        .where(member.id.eq(id))
+                        .fetchFirst()
+        );
+    }
+
+    @Override
     public boolean existsByNickname(String nickname) {
         return selectFrom(member)
                 .where(

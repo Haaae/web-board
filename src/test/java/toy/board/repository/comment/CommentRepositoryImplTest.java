ea65f69fa773dd7b9263b458481d557486823b3c
Haaae <101785628+Haaae@users.noreply.github.com>
2023-11-05 23:57:39 +0900

    add: @Trancstional 추가

--- src/test/java/toy/board/repository/comment/CommentRepositoryImplTest.java
+++ src/test/java/toy/board/repository/comment/CommentRepositoryImplTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 import toy.board.domain.auth.Login;
 import toy.board.domain.post.Comment;
 import toy.board.domain.post.CommentType;
@@ -20,6 +21,7 @@ import toy.board.domain.user.UserRole;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class CommentRepositoryImplTest {
 
     @Autowired

eee325e4c08a1a860dca7e2f2378b75f3bbcc2cf
김민규 <alsrbtls88@gmail.com>
2023-11-12 20:48:47 +0900

    feat: api 코드 에러 어노테이션 구현

---
+++ src/main/java/toy/board/controller/api/response/annotation/ApiAuthenticationError.java
@@ -0,0 +1,28 @@
+package toy.board.controller.api.response.annotation;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import toy.board.exception.ErrorResponse;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Documented
+@ApiResponse(
+        responseCode = "401",
+        description = "회원 인증 실패",
+        content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(
+                        implementation = ErrorResponse.class
+                )
+        )
+)
+public @interface ApiAuthenticationError {
+
+}

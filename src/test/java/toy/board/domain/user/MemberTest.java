d2af07ff9d1e4b337051587c60a6a87e34dcd265
김민규 <alsrbtls88@gmail.com>
2023-11-13 15:09:42 +0900

    delete: 불필요한 테스트 삭제

--- src/test/java/toy/board/domain/user/MemberTest.java
+++ src/test/java/toy/board/domain/user/MemberTest.java
@@ -1,8 +1,5 @@
 package toy.board.domain.user;
 
-import jakarta.persistence.EntityManager;
-import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import toy.board.domain.auth.Login;
@@ -10,21 +7,6 @@ import toy.board.domain.auth.Login;
 @SpringBootTest
 @Transactional
 public class MemberTest {
-
-    @Autowired
-    EntityManager em;
-
-    String username = "name";
-    Login login = Login.builder().encodedPassword("password").build();
-    Profile profile = Profile.builder("nickname").build();
-    LoginType loginType = LoginType.LOCAL_LOGIN;
-    UserRole userRole = UserRole.USER;
-    Member member;
-
-    @BeforeEach
-    void init() {
-        member = Member.builder(username, login, profile, loginType, userRole).build();
-    }
 
     public static Member create(String username, String nickname, UserRole role) {
         return Member.builder(

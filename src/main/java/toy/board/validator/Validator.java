8144e63da8e4d4b83ce79b93cac763810b88e492
김민규 <alsrbtls88@gmail.com>
2023-11-13 16:09:49 +0900

    feat: 도메인 객체 생성시 파라미터 검증 클래스 생성 및 적용

---
+++ src/main/java/toy/board/validator/Validator.java
@@ -0,0 +1,26 @@
+package toy.board.validator;
+
+import org.springframework.util.StringUtils;
+import toy.board.exception.BusinessException;
+import toy.board.exception.ExceptionCode;
+
+public class Validator {
+
+    public static void notNull(final Object object) {
+        if (object == null) {
+            throw new BusinessException(ExceptionCode.BAD_REQUEST_ARG);
+        }
+    }
+
+    public static void hasTextAndLength(final String text, final int maxLength) {
+        if (!StringUtils.hasText(text) || text.length() > maxLength) {
+            throw new BusinessException(ExceptionCode.BAD_REQUEST_ARG);
+        }
+    }
+
+    public static void hasText(final String text) {
+        if (!StringUtils.hasText(text)) {
+            throw new BusinessException(ExceptionCode.BAD_REQUEST_ARG);
+        }
+    }
+}
